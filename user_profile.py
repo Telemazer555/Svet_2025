954262ae79f5ed4a7d75f9b431ad43284f9c3638
Forsworn555 <forsworn555@yandex.ru>
2025-03-05 18:18:45 +0300

    https://seniorpomidorov.skillspace.ru/course/51665/932917#1
    Задание:
    Создайте переменные для хранения информации:
    Позвольте пользователю изменить данные через ввод.
    Добавьте проверку: если пользователь не ввел данные, программа выводит сообщение об ошибке.

---
+++ user_profile.py
@@ -0,0 +1,51 @@
+from platform import machine
+
+
+def info():
+    Triathlon = 300
+    print(f'Этого маловато для троеборья {Triathlon}')
+    while True:
+
+        try:
+            Squat = (input("Сколько присед?:"))
+            if not Squat:
+                Squat = 0
+            Squat = int(Squat)
+
+            chestpress = (input("Сколько жмёшь?:"))
+            if not chestpress:
+                chestpress = 0
+            chestpress = int(chestpress)
+
+            while True:
+                Deadlift = (input("Сколько тянешь?:"))
+                if not Deadlift:
+                    print('Тянуть надо полюбому')
+                else:
+                    try:
+                        Deadlift = int(Deadlift)
+                        break
+                    except ValueError:
+                        print("НУЖНЫ ЦИФРЫ!!!")
+                        continue
+
+        except ValueError:
+            print("Сума сдурел, циферки пиши, давай всё по новой")
+            continue
+
+        Triathlon = Squat + chestpress + Deadlift
+        print(f'Присед:  {Squat}')
+        print(f'Жим:  {chestpress}')
+        print(f'Тяга:  {Deadlift}')
+        print("Общая сумма троеборья:", Triathlon)
+
+        rembo = 1200
+        if rembo < Triathlon:
+            print('Ну ты точно калгон пьёшь')
+            break
+        else:
+            print(f"Иди качайся лалка, нужно набить: {rembo}")
+
+
+
+info()
