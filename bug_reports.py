563a7600d17c6e134e437727d7373fd0f748b042
Forsworn555 <forsworn555@yandex.ru>
2025-03-10 10:08:34 +0300

    6. Коллекции данных  https://seniorqadenis.notion.site/4-0-Fullstack-Python-1628d6cd33d0804d9624e88da4f831bc **Результат:**
    Программа `bug_reports.py`, которая:
    
    - Работает со списком багов.
    - Реализует добавление, удаление и сортировку.
    
    Пробую смотреть че там с list comprehension

---
+++ bug_reports.py
@@ -0,0 +1,10 @@
+bug_reports = ["Ошибка 1 — High","Ошибка 2 — Low","Ошибка 3 — High","Ошибка 4 — Medium","Ошибка 5 — Medium","Ошибка 33 — Low"]
+print(bug_reports)
+a = input('Какой баг добавить ? ')
+bug_reports.append(a)
+print(bug_reports)
+bug_reports.sort()
+print(bug_reports)
+x = input('Какой баг удалить ? (может Low): ')
+bug_reports = [r for r in bug_reports if x not in r]
+print(bug_reports)
